c9819c160a5761f024e1aaf02bb4bb099e662dad
Omkar Kambire <omkar.yupgp22@sinhgad.edu>
2025-10-08 15:29:59 +0530

    Like, Unlike and Edit post

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const post = require("./models/post");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
@@ -25,6 +26,33 @@ app.get("/profile", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({email: req.user.email}).populate("posts");
     res.render("profile", {user});
 })
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+
+    if(post.likes.indexOf(req.user.userId) === -1){
+      post.likes.push(req.user.userId);
+    }
+    else{
+      post.likes.splice(post.likes.indexOf(req.user.userId), 1);
+    }
+    await post.save();
+    res.redirect("/profile");
+})
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({_id: req.params.id}).populate("user");
+
+  res.render("edit", {post});
+
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content}, {new: true})
+
+  res.redirect("/profile");
+
+});
 
 app.post("/post", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({email: req.user.email});
